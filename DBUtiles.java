a86d3ae49cd943374556e1d9cddd10df40b840be
Thauti57 <thautichandrasheker@gmail.com>
2022-10-29 09:32:04 +0530

    direct working area files moving to the local Repo

--- DBUtiles.java
+++ DBUtiles.java
@@ -1,3 +1,4 @@
  Hi This is DBUtiles java file
  please do practice the git commands
- Hi adding one more line to chcek the froim local repo to working area 
+ Hi adding one more line to chcek the froim local repo to working area
+ adding one line to check the git reset : git reset will do staging area files shifted to the working area 
