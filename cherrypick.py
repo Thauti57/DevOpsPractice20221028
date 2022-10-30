055aca5a6f1894c7b3237adb491258941138cfc7
Thauti57 <thautichandrasheker@gmail.com>
2022-10-30 14:39:05 +0530

    cherrypic commitIDcherrypick.py

--- cherrypick.py
+++ cherrypick.py
@@ -1,3 +1,5 @@
 Hi this is cherrypick test file
 
-Just want to check the cherry-pick command how it will effect the file. 
+Just want to check the cherry-pick command how it will effect the file.
+
+add oine more line to add commit message
